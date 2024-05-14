3387149b08f417fdb0b111d7e4e6af2e0327a011
Igor Kopyshev <kopyshev.se@gmail.com>
2024-05-14 13:08:38 +0400

    Initial commit

---
+++ src/main/java/edu/basejava/HelloJava.java
@@ -0,0 +1,13 @@
+package edu.basejava;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloJava {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloJava.class);
+
+    public static void main(String[] args) {
+        LOG.debug("Hello World");
+    }
+}
